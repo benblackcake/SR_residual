93c607c134fade228d62f8540d45fcaf758d9de2
ben <pingheng.chang@gmail.com>
2020-04-08 14:53:01 +0800

    testing model structure

--- model.py
+++ model.py
@@ -14,14 +14,15 @@ class SRresidual:
 		with tf.variable_scope('residual') as scope:
 			x = tf.layers.conv2d(x, kernel_size=3, filters=1, strides=1, padding='same', use_bias=False)
 			x = tf.nn.relu(x)
-			# skip = x
+			skip = x
 			for i in range(n_layer-1):
 				# w = self._weight(shape=[3,3,3,64])
 				x = self._conv_layer(x)
 				print(x)
 			x = tf.layers.conv2d(x, kernel_size=3, filters=1, strides=1, padding='same', use_bias=False)
-			x = tf.nn.relu(x)
-			# x = x + skip
+			# x = tf.nn.relu(x)
+			x = tf.layers.batch_normalization(x, training=self.is_train)
+			x = x + skip
 
 			return x
 
